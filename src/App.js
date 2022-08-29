dca77e16b2c0335790a78dea5a4fbda941f1cb64
Can Berk Ocalir <can.ocalir@gmail.com>
2022-08-30 02:55:42 +0300

    fixed off digits

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import CELLS from "vanta/dist/vanta.cells.min.js";
 
 function App() {
   const toLocaleString = (num) =>
-    String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ");
+    String(num).replace(/(?<!\..*)(\d)(?=(?:\d{12})+(?:\.|$))/g, "$1 ");
 
   const removeSpaces = (num) => num.toString().replace(/\s/g, "");
 
--- src/components/Screen/Screen.js
+++ src/components/Screen/Screen.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import NumericLabel from 'react-pretty-numbers'
 import './Screen.css'
 
 const Screen = ({value}) => {
+
+  let params = {
+    commafy: true,
+  }
+
   return (
     <div className="screen">
-        <span>{value}</span>
+       <span><NumericLabel params={params}>{value}</NumericLabel></span>
     </div>
   )
 }
